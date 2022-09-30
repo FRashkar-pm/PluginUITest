5150bcc8e7b2829fff21d6ee9358ac75161a61e7
FRashkar-C <114340217+FRashkar-pm@users.noreply.github.com>
2022-09-30 09:33:00 +0700

    Update PluginUITest.php

--- PluginUITest/src/FRashkar/PluginUITest.php
+++ PluginUITest/src/FRashkar/PluginUITest.php
@@ -30,7 +30,7 @@ class PluginUITest extends PluginBase implements Listener {
         return true;
     }
     public function ui(Player $player) {
-        $form = new CustomForm(function(Player $player, int $data = null){
+        $form = new createCustomForm(function(Player $player, int $data = null){
             if($data === null){
                 return;
             }
